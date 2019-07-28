789393aee1512cdf0adc63bca82583b6834e9553
chenkang123456 <2447336527@qq.com>
2019-07-28 12:14:19 +0800

    java基本语法

---
+++ javatest/demo.java
@@ -0,0 +1,15 @@
+public class demo{
+    public static void main(String[] args) {
+        int weekofday=0;
+        switch(weekofday){
+            case 0:
+               System.out.println("星期日");
+               break;
+            case 1:
+                System.out.println("星期一");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ javatest/text04.java
@@ -0,0 +1,30 @@
+import com.oracle.jrockit.jfr.ValueDefinition;
+
+public class text04{
+    public static void main(String[] args) {
+
+//整型（int）数 97 代表的字符是什么？
+
+     int i=97;
+     String s = "" + i;
+     System.out.println(s);
+
+     
+
+
+//3)  字符‘0’代表整型数是多少？
+/*
+        char c2= '0';
+
+        int b2=c2;
+        
+        System.out.println(b2);
+        */
+//4)  ‘\u0041’代表的字符是什么？
+        
+
+ 
+     
+      
+    }
+}
